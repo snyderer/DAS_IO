a64710f04a7a273ba3d91af337f729bb32c2be1e
esnyder <esnyder@cornell.edu>
2026-02-02 20:18:44 -0500

    functioning version

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="das_io",
+    version="0.1.0",
+    packages=find_packages(where="src"),  # look in src/
+    package_dir={"": "src"},               # root for packages is src/
+    install_requires=open("requirements.txt").read().splitlines(),                   # read requirements.txt here if you want
+)
---
+++ src/das_io/__init__.py
@@ -0,0 +1,6 @@
+from .data_io import load_tx, find_settings_h5, load_settings_preprocessed_h5  # or import specific functions/classes
+
+# allow users to import specific functions:
+__all__ = ["load_tx", "find_settings_h5", "load_settings_preprocessed_h5"] 
+
+__version__ = "0.1.0"
