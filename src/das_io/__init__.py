91d6d79663de735b3e1b1ab204e129ae054eb118
Dr. Eric R. Snyder <esnyder@cornell.edu>
2026-03-17 17:11:26 -0400

    Add dehydrate functions to das_io module

--- src/das_io/__init__.py
+++ src/das_io/__init__.py
@@ -1,4 +1,4 @@
-from .data_io import load_tx, find_settings_h5, load_settings_preprocessed_h5  # or import specific functions/classes
+from .data_io import load_tx, find_settings_h5, load_settings_preprocessed_h5, dehydrate_fk, dehydrate_tx  # or import specific functions/classes
 
 # allow users to import specific functions:
 __all__ = ["load_tx", "find_settings_h5", "load_settings_preprocessed_h5", "dehydrate_fk", "dehydrate_tx"] 
