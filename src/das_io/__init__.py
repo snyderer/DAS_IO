fb471db33b49e233247a4a2d74acdac4c51c38f5
Dr. Eric R. Snyder <esnyder@cornell.edu>
2026-03-17 17:27:09 -0400

    Add rehydrate function to module exports

--- src/das_io/__init__.py
+++ src/das_io/__init__.py
@@ -1,6 +1,6 @@
-from .data_io import load_tx, find_settings_h5, load_settings_preprocessed_h5, dehydrate_fk, dehydrate_tx  # or import specific functions/classes
+from .data_io import load_tx, find_settings_h5, load_settings_preprocessed_h5, rehydrate, dehydrate_fk, dehydrate_tx  # or import specific functions/classes
 
 # allow users to import specific functions:
-__all__ = ["load_tx", "find_settings_h5", "load_settings_preprocessed_h5", "dehydrate_fk", "dehydrate_tx"] 
+__all__ = ["load_tx", "find_settings_h5", "load_settings_preprocessed_h5", "rehydrate", "dehydrate_fk", "dehydrate_tx"] 
 
 __version__ = "0.1.0"
